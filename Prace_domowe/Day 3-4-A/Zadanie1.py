e56291f6038e5f28794ff58ff985f3a7981ed217
Slawecky <comsitecomputer@gmail.com>
2017-09-19 14:13:31 +0200

    19-09

--- Prace_domowe/Day 3-4-A/Zadanie1.py
+++ Prace_domowe/Day 3-4-A/Zadanie1.py
@@ -1,3 +1,17 @@
 #   sprawdź czy jest wygrana w kółko i krzyżyk
 #   input: string 9 znaków x, o, n
 #   znaki oznaczają pozycje wierszami od górnego
+
+
+gra = input("Podaj wynik gry w k&k X, 0, N ( 9 znaków bez spacji przecinków itp. : ")
+if len(gra) == 9:
+# poziomo
+    if gra.index("XXX") == 0:
+        print("Wygrana X")
+        if gra.index(""):
+            print(" Coś nie haloo")
+
+
+print(" Dorobię kiedy indziej")
+
+# Do sprawdzenia czy koncepcja jest OK
--- Prace_domowe/Day 3-4-A/Zadanie4.py
+++ Prace_domowe/Day 3-4-A/Zadanie4.py
@@ -1,6 +1,22 @@
-<<<<<<< HEAD
-# oblicz ocenę na podstawie progu procentowego
+# oblicz ocenę na podstawie progu procent
+# Kryterium ocen z AWF'u
+# 30% - 40% 2, 41%-60% 3, 61% - 78% 4, 79% - 94% 5, 95% -100% 6
 
-=======
-# oblicz ocenę na podstawie progu procentowego
->>>>>>> master
+ocena = " Słabiutko"
+wynik = int(input( "Podaj swój wynik procentowy i nie wtryniaj literek bo się program wywali : "))
+
+if wynik < 30:
+    ocena = " Ucz się dalej !"
+elif wynik <= 40:
+    ocena = " Pała trochę lepiej niż w zeszłym roku"
+elif wynik <= 60:
+    ocena = " Trója brawo !"
+elif wynik <= 78:
+    ocena = " Czwórka ? A łyżka na to niemożliwe !"
+elif wynik <= 94:
+    ocena = " Piątka ? mama za Ciebie pisała ?"
+elif wynik <= 100:
+    ocena = " Szóstka ? Daj mi pospać jeszcze pół godziny i potem obudź"
+print(f"Hmm {wynik} % to {ocena}")
+
+# Standardowo zabezpieczenia input error kiedy indziej
