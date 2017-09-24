cae8b7d1eee19957ddea799c3c2059e5aebede59
Slawecky <comsitecomputer@gmail.com>
2017-09-24 18:43:59 +0200

    Pierwszy etap

---
+++ Prace_domowe/Day_5-6/uczelnia.py
@@ -0,0 +1,12 @@
+# Program - baza nowych studentów
+# Opcje do wyboru
+
+# 1. Dodaj studenta
+# 2. Usuń studenta
+# 3. Wyszukaj studenta
+# 4. Pokaż listę studentów
+# ...
+# x. zakończ program
+
+# Wpis w bazie danych : 1 - Imię, 2-Nazwisko, 3- Pesel, 4-Ocena kwalifikacyjna
+
