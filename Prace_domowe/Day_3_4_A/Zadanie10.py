ae145942b5005b70799e96ca9d8d4b65dccc9c9b
Slawecky <comsite@comsite.com.pl>
2017-10-01 16:07:57 +0200

    Snuś2

--- Prace_domowe/Day_3_4_A/Zadanie10.py
+++ Prace_domowe/Day_3_4_A/Zadanie10.py
@@ -1,5 +1,15 @@
- zamiana temperatury
+#     zamiana temperatury
 #     wejscie: "35C" "100F"
 #     wyjście "Temperatura w {typ} to {xxx} stopni"
 #     C = (F - 32) * (5/9)
 #     F = C * (9 / 5) + 32
+
+celsjusz = input("Podaj temperaturę w Celsjuszach :")
+fahr = input("Podaj temperaturę w stopniach Fehrenheita :")
+
+print(f"Podałeś : {celsjusz} stopni Celsjusza i {fahr} stopni Fahrenheita")
+
+
+exit(
+
+)
---
+++ Prace_domowe/Day_3_4_A/Zadanie10_s.py
@@ -0,0 +1,19 @@
+# Synuś w c
+#     C = (F - 32) * (5/9)
+#     F = C * (9 / 5) + 32
+
+startc = int(0)
+startf = int(0)
+stopc = int(300)
+stopf = int(300)
+krok = int(5)
+
+print("\nFahrenheit Celsjusz\n")
+while startc <= stopc:
+    print(startc, "         ", round((startc-32)*(5/9), 3))
+    startc = startc + krok
+print("\nCelsjusz Fahrenheit\n ")
+
+while startf <= stopf:
+    print(startf, "          ", round((startf*(5/9) + 32), 3))
+    startf = startf + krok
