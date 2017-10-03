cdb2214713b169fd404325e06c8c89f055fd0018
Slawecky <comsite@comsite.com.pl>
2017-10-03 12:07:01 +0200

    workspace

--- Prace_domowe/Day_3_4_A/Zadanie10_s.py
+++ Prace_domowe/Day_3_4_A/Zadanie10_s.py
@@ -2,7 +2,7 @@
 #     C = (F - 32) * (5/9)
 #     F = C * (9 / 5) + 32
 
-startc = int(0)
+startc = int(input("Podaj : "))
 startf = int(0)
 stopc = int(300)
 stopf = int(300)
@@ -10,10 +10,12 @@ krok = int(5)
 
 print("\nFahrenheit Celsjusz\n")
 while startc <= stopc:
-    print(startc, "         ", round((startc-32)*(5/9), 3))
+    print(startc, "         ", round((startc-32)*(5/9), 1))
     startc = startc + krok
 print("\nCelsjusz Fahrenheit\n ")
 
 while startf <= stopf:
     print(startf, "          ", round((startf*(5/9) + 32), 3))
     startf = startf + krok
+
+# Ślązak
